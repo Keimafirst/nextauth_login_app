f6d0a777110f4e950a880298a4fa702c7743e740
Keimafirst <test007_griffine_pulas@yahoo.co.jp>
2025-06-28 19:46:32 +0900

    認証処理の実装 #2

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,48 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { compare } from "bcryptjs";
+import { prisma } from "@/lib/prisma";
+
+/**
+ * 資格情報（E-mail・Password）による認証を定義
+ */
+export const authOptions: NextAuthOptions = {
+  providers: [
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // E-mail・Passwordの入力チェック
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+        // emailでユーザー情報を検索 emailはユニークで定義
+        const user = await prisma.users.findUnique({
+          where: { email: credentials?.email },
+        });
+        if (!user) return null;
+
+        // 入力PWと複合PWwを照合
+        const isValid = await compare(credentials!.password, user.password);
+        if (isValid) {
+          return {
+            id: user.id.toString(),
+            name: user.username,
+            email: user.email,
+          };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  pages: { signIn: "/login" },
+  session: { strategy: "jwt" },
+  jwt: {},
+  secret: process.env.NEXTAUTH_SECRET,
+};
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
